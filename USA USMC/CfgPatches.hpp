380a4ce02e5caf2b3b25c572d34058cda6da5d60
Muhammad Akhta <akhtacaesar@gmail.com>
2024-04-20 10:46:48 +0700

    Added USMC Infantry

---
+++ USA USMC/CfgPatches.hpp
@@ -0,0 +1,47 @@
+class CfgPatches {
+    class AKO_US_USMC {
+        units[] = {
+            "us_usmc_sergeant",
+            "us_usmc_lance_corporal",
+            "us_usmc_rifleman_m16",
+            "us_usmc_rifleman_m4a1",
+            "us_usmc_rifleman_at",
+            "us_usmc_autorifleman_m249",
+            "us_usmc_machinegunner",
+            "us_usmc_marksman",
+            "us_usmc_sniper",
+            "us_usmc_corpsman",
+            "us_usmc_engineer",
+            "us_usmc_aa_specialist",
+            "us_usmc_missileman",
+            "us_usmc_officer",
+            "us_usmc_crewman",
+            "us_usmc_worker",
+            "us_usmc_heli_pilot",
+            "us_usmc_jet_pilot",
+            "us_usmc_marsoc_chief",
+            "us_usmc_marsoc_heli_pilot",
+            "us_usmc_marsoc_eod",
+            "us_usmc_marsoc_marksman",
+            "us_usmc_marsoc_m4a4",
+            "us_usmc_marsoc_mk16",
+            "us_usmc_marsoc_mk17",
+            "us_usmc_marsoc_m249",
+            "us_usmc_marsoc_eglm"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "rhsusf_c_troops",
+            "A3_Characters_F",
+            "A3_Sounds_F",
+            "A3_Sounds_F_Exp",
+            "A3_Data_F",
+            "A3_Data_F_Curator",
+            "A3_Air_F_Heli",
+            "cba_xeh"
+        };
+        author = "Akosovski";
+        authors[] = {"Akosovski"};
+    };
+};
---
+++ USA USMC/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT akoususmc
+#define COMPONENT_BEAUTIFIED akoususmc
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_AKOUSUSMC
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_AKOUSUSMC
+#define DEBUG_SETTINGS DEBUG_SETTINGS_AKOUSUSMC
+#endif
+#include "\x\\addons\main\script_macros.hpp"
