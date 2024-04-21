03bc7268bc040c220835c9067eae383863c33244
Muhammad Akhta <akhtacaesar@gmail.com>
2024-04-21 20:52:51 +0700

    ADF Addition

---
+++ ALT ADF/CfgPatches.hpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+    class AL_ADF {
+        units[] = {
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+        };
+        author = "Akosovski";
+        authors[] = {"Akosovski"};
+    };
+};
---
+++ ALT ADF/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT aladf
+#define COMPONENT_BEAUTIFIED aladf
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_ALADF
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_ALADF
+#define DEBUG_SETTINGS DEBUG_SETTINGS_ALADF
+#endif
+#include "\x\\addons\main\script_macros.hpp"
